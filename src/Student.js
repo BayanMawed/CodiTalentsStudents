3af49f53ca3f94b65b9dd37e8b697f2e4d9dab5c
amrgharz <ammor.ghr@gmail.com>
2018-05-02 11:59:54 +0300

    CSS and student.js  changes

--- src/Student.js
+++ src/Student.js
@@ -7,12 +7,17 @@ const Student = ({gender,name,availability,skills,city,OnClickAdd,image}) => (
       <Thumbnail >
         {/* <Image src={image} width="150px" height="150px" circle /> */}
         <div className='student-image' style={{ backgroundImage: `url(${image})` }} > </div>
-        <h2 className="student-name">{name}</h2>
-        <h3>City: {city}</h3>
-        <h3>Availability: </h3>
-        <p>{availability}</p>
-        <h3>Skills: </h3>
-        <p>#{skills.join('#')}</p>
+        <div className='student-info'>
+          <h2 className="student-name">{name}</h2>
+          <h3 className="student-city">City: </h3>
+          <p>{city}</p>
+          <h3 className='student-availability'>Availability: </h3>
+          <p>{availability}</p>
+          <h3 className='student-skills'>Skills: </h3>
+          <div className="skills">
+            <p>#{skills.join('#')}</p>
+          </div>
+        </div>
         <hr />
         <p>
         {/* <Button bsStyle="primary">See More</Button>&nbsp; */}
